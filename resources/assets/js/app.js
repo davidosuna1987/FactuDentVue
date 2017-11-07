7885ba0382139ff54c3825954972e9e048fcc67a
David <david@supermundano.com>
2017-11-07 19:01:17 +0100

    First commit

---
+++ resources/assets/js/app.js
@@ -0,0 +1,12 @@
+require('./bootstrap');
+
+window.Vue = require('vue');
+import Buefy from 'buefy';
+
+Vue.use(Buefy);
+// Vue.component('example', require('./components/Example.vue'));
+
+var app = new Vue({
+	el: '#app',
+	data: {}
+})
