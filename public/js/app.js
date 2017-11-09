eafd36c75b2783dfc109524646321e76bd7a4055
David <david@supermundano.com>
2017-11-09 12:16:07 +0100

    Initial settings & form login and register

--- public/js/app.js
+++ public/js/app.js
@@ -979,12 +979,40 @@ __webpack_require__(11);
 window.Vue = __webpack_require__(34);
 
 
-Vue.use(__WEBPACK_IMPORTED_MODULE_0_buefy___default.a);
+Vue.use(__WEBPACK_IMPORTED_MODULE_0_buefy___default.a, {
+  defaultIconPack: 'fa'
+});
+
 // Vue.component('example', require('./components/Example.vue'));
 
 var app = new Vue({
-	el: '#app',
-	data: {}
+  el: '#app',
+  data: {}
+});
+
+// BULMA responsive menú open and close
+document.addEventListener('DOMContentLoaded', function () {
+
+  // Get all "navbar-burger" elements
+  var $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+
+  // Check if there are any navbar burgers
+  if ($navbarBurgers.length > 0) {
+
+    // Add a click event on each of them
+    $navbarBurgers.forEach(function ($el) {
+      $el.addEventListener('click', function () {
+
+        // Get the target from the "data-target" attribute
+        var target = $el.dataset.target;
+        var $target = document.getElementById(target);
+
+        // Toggle the class on both the "navbar-burger" and the "navbar-menu"
+        $el.classList.toggle('is-active');
+        $target.classList.toggle('is-active');
+      });
+    });
+  }
 });
 
 /***/ }),
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,7 +1,80 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
+
+<section class="guest-form">
+    <div class="background" style="background-image: url({{ asset('images/backgrounds/gabinete-herramientas.jpg')  }});"></div>
+    <div class="form-container">
+        <form class="form" method="POST" action="{{ route('register') }}" role="form">
+            {{ csrf_field() }}
+
+            <div class="field{{ $errors->has('name') ? ' has-error' : '' }}">
+                <label for="name" class="label is-small">Nombre</label>
+
+                <p class="control has-icons-left">
+                    <input id="name" type="text" class="input is-small{{ $errors->has('name') ? ' is-danger' : ' is-primary' }}" name="name" value="{{ old('name') }}" required autofocus>
+                    <span class="icon is-small is-left"><i class="fa fa-user"></i></span>
+
+                    @if($errors->has('name'))
+                        <p class="help is-danger">{{ $errors->first('name') }}</p>
+                    @endif
+                </p>
+            </div>
+
+            <div class="field{{ $errors->has('email') ? ' has-error' : '' }}">
+                <label for="email" class="label is-small">E-Mail</label>
+
+                <p class="control has-icons-left">
+                    <input id="email" type="email" class="input is-small{{ $errors->has('email') ? ' is-danger' : ' is-primary' }}" name="email" value="{{ old('email') }}" required autofocus>
+                    <span class="icon is-small is-left"><i class="fa fa-at"></i></span>
+
+                    @if($errors->has('email'))
+                        <p class="help is-danger">{{ $errors->first('email') }}</p>
+                    @endif
+                </p>
+            </div>
+
+            <div class="field{{ $errors->has('password') ? ' has-error' : '' }}">
+                <label for="password" class="label is-small">Contraseña</label>
+
+                <p class="control has-icons-left">
+                    <input id="password" type="password" class="input is-small{{ $errors->has('password') ? ' is-danger' : ' is-primary' }}" name="password" required>
+                    <span class="icon is-small is-left"><i class="fa fa-lock"></i></span>
+
+                    @if($errors->has('password'))
+                        <p class="help is-danger">{{ $errors->first('password') }}</p>
+                    @endif
+                </p>
+            </div>
+
+            <div class="field{{ $errors->has('password_confirmation') ? ' has-error' : '' }}">
+                <label for="password_confirmation" class="label is-small">Confirmar contraseña</label>
+
+                <p class="control has-icons-left">
+                    <input id="password_confirmation" type="password" class="input is-small{{ $errors->has('password_confirmation') ? ' is-danger' : ' is-primary' }}" name="password_confirmation" required>
+                    <span class="icon is-small is-left"><i class="fa fa-lock"></i></span>
+
+                    @if($errors->has('password_confirmation'))
+                        <p class="help is-danger">{{ $errors->first('password_confirmation') }}</p>
+                    @endif
+                </p>
+            </div>
+
+            <b-checkbox name="remember" class="label is-small">Recuérdame</b-checkbox>
+
+            <div class="control">
+                <button class="button is-primary is-small is-outlined m-t-10">Entrar</button>
+            </div>
+
+            <p class="content is-small is-pulled-right">
+                <a class="is-muted" href="{{ route('login') }}">¿Ya estás registrado? ¡Entra!</a>
+            </p>
+        </form>
+    </div>
+</section>
+
+
+{{-- <div class="container">
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
@@ -73,5 +146,5 @@
             </div>
         </div>
     </div>
-</div>
+</div> --}}
 @endsection
